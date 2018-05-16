bdbf2c081ce03b38a9fd3245ed16ac43ecc58e6f
MicroIMOOC <1434246346@qq.com>
2018-05-16 22:28:56 +0800

    Mod 底部

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -5,6 +5,7 @@
 				<div class="footer__inner">
 					<div class="footer__region">
 						<li><a href="#">QQ： 1434246346</a></li>
+						<li><a href="#">手机: 17865358263<a></li>
 					</div>
 					<div class="footer__secondary__nav">
 						<span>Copyright © 2018 JIé∬ All Rights Reserved.</span>
